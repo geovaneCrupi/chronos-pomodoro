a77caedf6097920a405153592b165e96690a43f0
Geovane Crupi <geovane.crupi@gmail.com>
2026-05-07 15:40:24 -0300

    Aula 21 - Movendo conainer para React component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Heading } from "./components/Heading";
-import { TimerIcon } from "lucide-react";
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 import './styles/themes.css';
 import './styles/global.css';
@@ -8,13 +8,12 @@ import './styles/global.css';
 export function App() {
     return (
         <>
-            <Heading>
-                Olá mundo 1
-                <button>
-                    <TimerIcon />
-                </button>
-            </Heading>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolorum illo aut, vitae, nobis velit soluta nam voluptatibus fugiat odio molestias, corrupti nisi sit enim rerum temporibus necessitatibus dolores optio.</p>
+            <Container>
+                <Heading>LOGO</Heading>
+            </Container>
+            <Container>
+               <Heading>MENU</Heading>
+            </Container>
         </>
     )
 }
