2f14bce5fc7d5988818f4dd5be67af20b70cc448
Geovane Crupi <geovane.crupi@gmail.com>
2026-05-07 17:26:30 -0300

    Aula 26 - Criação do Formulário

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Logo } from './components/Logo';
 
 import './styles/themes.css';
 import './styles/global.css';
+import { CountDown } from './components/CountDown';
 
 
 export function App() {
@@ -14,6 +15,27 @@ export function App() {
             </Container>
             <Container>
                <Menu />
+            </Container>
+            <Container>
+                <CountDown />
+            </Container>
+            <Container>
+                <form action="" className="form">
+                    <div className="formRow">
+                        <label htmlFor="meuInput">Task</label>
+                        <input type="text" id="meuInput" />
+                    </div>
+                    <div className="formRow">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div className="formRow">
+                        <p>Ciclos</p>
+                        <p>0 0 0 0 0</p>
+                    </div>
+                    <div className="formRow">
+                        <button>Enviar</button>
+                    </div>
+                </form>
             </Container>
         </>
     )
