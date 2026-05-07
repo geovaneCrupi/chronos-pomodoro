0e739bcd7ceb636b688a0cd7a19c0ec5d7e7365c
Geovane Crupi <geovane.crupi@gmail.com>
2026-05-07 14:34:17 -0300

    Aula 18 - Tipagem typeScript de children (ReactNode)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Heading } from "./components/Heading";
+import { TimerIcon } from "lucide-react";
 
 import './styles/themes.css';
 import './styles/global.css';
@@ -7,7 +8,12 @@ import './styles/global.css';
 export function App() {
     return (
         <>
-            <Heading attr={123} attr2='String'>Olá mundo 1</Heading>
+            <Heading>
+                Olá mundo 1
+                <button>
+                    <TimerIcon />
+                </button>
+            </Heading>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolorum illo aut, vitae, nobis velit soluta nam voluptatibus fugiat odio molestias, corrupti nisi sit enim rerum temporibus necessitatibus dolores optio.</p>
         </>
     )
