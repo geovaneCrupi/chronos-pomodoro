5ce981d27a860f0f296a51d4a6f6e238a90e3051
Geovane Crupi <geovane.crupi@gmail.com>
2026-05-18 16:09:14 -0300

    Aula 38 - useEffect e Função de CleanUp

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,6 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 type AvailableThemes = 'dark' | 'light';
 
@@ -12,15 +12,30 @@ export function Menu() {
         event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
     ) {
         event.preventDefault(); // Não segue o link
-        console.log('Clicado', Date.now());
 
         setTheme(prevTheme => {
             const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
             return nextTheme;
-        })
-
-        //document.documentElement.setAttribute('data-theme' , theme);
+        });
     }
+
+    // useEffect(() => {
+    //     console.log('useEffect sem dependências', Date.now());
+    // }); // Executado toda vez que o componente renderiza na tela
+
+    // useEffect(() => {
+    //     console.log('useEffect com array dependencias vazio', Date.now());
+    // }, []); // Executa apenas quando o React monta o componente na tela pela primeira vez
+
+    useEffect(() => {
+        console.log('Theme mudou', theme, Date.now());
+        document.documentElement.setAttribute('data-theme', theme);
+
+        return () => {
+            console.log('Olha, este componente será atualizado'); // função de CleanUp
+        };
+
+    }, [theme]); // Executa apenas quando o valor de dependendencias (theme) muda.
 
     return (
     <div className={styles.menu}>
