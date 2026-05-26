abfe2f66846f7f4e39739e4959b521b18d511408
Geovane Crupi <geovane.crupi@gmail.com>
2026-05-26 18:38:54 -0300

    Aula 54 - iniciando as config do form

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,21 +3,55 @@ import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 import { useRef, useState } from "react";
+import type { TaskModel } from "../../models";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 
 export function MainForm() {
 
-    const [taskName, setTaskName] = useState('');
+    const { setState } = useTaskContext();
+
     const taskNameInput = useRef<HTMLInputElement>(null);
 
-    function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    function handleCreateNewTask(event: React.SubmitEvent<HTMLFormElement>) {
         event.preventDefault();
-        console.log('DEU CERTO', taskName, taskNameInput.current.value);
+        
+        if(taskNameInput.current === null) return;
+
+        const taskName = taskNameInput.current.value.trim();
+
+        if(!taskName) {
+            alert("Digite o nome da tarefa!!!!");
+            return;
+        }
+
+        const newTask: TaskModel = {
+            id: Date.now().toString(),
+            name: taskName,
+            startDate: Date.now(),
+            completeDate: null,
+            interruptDate: null,
+            duration: 1,
+            type: 'workTime',
+        };
+
+        const secondsRemaining = newTask.duration * 60;
+
+        setState( prevState => {
+            return {
+                ...prevState,
+                config: { ...prevState.config },
+                activeTask: newTask,
+                currentCycle: 1, //Conferir
+                secondsRemaining,
+                formattedSecondsRemaining: '00:00', //conferir,
+                tasks: [...prevState.tasks, newTask],
+            }
+        })
     }
 
     return (
         <form action="" onSubmit={handleCreateNewTask} className="form">
             <div className="formRow">
-                <h1>{taskName}</h1>
                 <DefaultInput 
                 labelText='task' 
                 id="meuInput" 
