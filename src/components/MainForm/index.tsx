14319b6044dde363cb548474d8d9c123c8fb3858
Geovane Crupi <geovane.crupi@gmail.com>
2026-05-26 16:50:51 -0300

    Aula 51 e 52 - evento submit onChange e useState

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,13 +2,28 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
+import { useState } from "react";
 
 export function MainForm() {
 
+    const [taskName, setTaskName] = useState('');
+
+    function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        console.log('DEU CERTO');
+    }
+
     return (
-        <form action="" className="form">
+        <form action="" onSubmit={handleCreateNewTask} className="form">
             <div className="formRow">
-                <DefaultInput labelText='task' id="meuInput" placeholder='Digite uma tarefa' type="text" />
+                <h1>{taskName}</h1>
+                <DefaultInput 
+                labelText='task' 
+                id="meuInput" 
+                placeholder='Digite uma tarefa' 
+                type="text" 
+                value= {taskName}
+                onChange={e => setTaskName(e.target.value)} />
             </div>
             <div className="formRow">
                 <p>Próximo intervalo é de 25min</p>
