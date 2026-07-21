eec8eb3552f9bbf8a92c4861c4b59107320efbb1
Geovane Crupi <geovane.crupi@gmail.com>
2026-07-21 18:38:49 -0300

    Aula 69 - Componente Tips (sugestões para cada tarefa)

---
+++ src/components/Tips/index.tsx
@@ -0,0 +1,29 @@
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
+
+export function Tips() {
+    const { state } = useTaskContext();
+    const nextCycle = getNextCycle(state.currentCycle);
+    const nextCycleType = getNextCycleType(nextCycle);
+
+    // Tips
+    const tipsForWhenActiveTask = {
+        workTime: <span>Foque por <b>{state.config.workTime}</b> min</span>,
+        shortBreakTime: <span>Descanse por <b>{state.config.shortBreakTime} min</b></span>,
+        longBreakTime: <span>Descanso longo</span>,
+    }
+
+    const tipsForNoActiveTask = {
+        workTime: <span>Próximo ciclo é de <b>{state.config.workTime} min</b></span>,
+        shortBreakTime: <span>Próximo descanso é de <b>{state.config.shortBreakTime} min</b></span>,
+        longBreakTime: <span>Próximo descanso será longo</span>,
+    }
+
+    return(
+        <>
+            {!!state.activeTask && tipsForWhenActiveTask[state.activeTask.type]}
+            {!state.activeTask && tipsForNoActiveTask[nextCycleType]}
+        </>
+    );
+}
